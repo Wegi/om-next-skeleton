7db79b639ebde39c1aded0931b4b9699a793a326
Alexander Schneider <alexander@schneider.gg>
2018-07-13 16:11:59 +0200

    remove compiled js

--- resources/public/js/argyou_homepage/core.cljs
+++
@@ -1,9 +0,0 @@
-(ns argyou-homepage.core
-  (:require [goog.dom :as gdom]
-            [om.next :as om :refer-macros [defui]]
-            [om.dom :as dom]))
-
-(enable-console-print!)
-
-(println "Hello World!")
-
